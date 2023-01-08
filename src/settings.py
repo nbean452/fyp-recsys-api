c14f416628d17f32135c868b965736b76e0cdbcc
Nicholas Benedict <19086235d@connect.polyu.hk>
2023-01-08 23:57:11 +0700

    deploy test

--- src/settings.py
+++ src/settings.py
@@ -27,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-IS_PROD = os.getenv('ENVIRONMENT') == 'DEVELOPMENT'
+IS_PROD = os.getenv('ENVIRONMENT') == 'PRODUCTION'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = IS_PROD
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.nbenedictcodes.com']
 
 
 # Application definition
@@ -188,6 +188,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
