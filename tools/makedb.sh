b4728f9e0eb5810067a7f8d76ece5106bd2bfeb8
Nicholas Benedict <nicholasb1537@gmail.com>
2023-01-25 21:26:46 +0700

    update shell script

--- tools/makedb.sh
+++ tools/makedb.sh
@@ -1,1 +1,1 @@
-trash db.sqlite3 && trash base/migrations/ && pipenv run python manage.py makemigrations base && pipenv run python manage.py migrate && pipenv run loaddata && pipenv run python manage.py createsuperuser --noinput
+rm db.sqlite3 && rm -rf base/migrations/ && pipenv run python manage.py makemigrations base && pipenv run python manage.py migrate && pipenv run loaddata && pipenv run python manage.py createsuperuser --noinput
